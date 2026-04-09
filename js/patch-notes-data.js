8f61f9beace7e5f0a4b21ea794c13502bf7ca036
[imjunseoo] <imjunse040205@gmail.com>
2026-04-09 22:22:51 +0900

    docs: update patch notes for v0.1.1 (leaderboard update)

--- js/patch-notes-data.js
+++ js/patch-notes-data.js
@@ -6,6 +6,7 @@ const PATCH_NOTES_DATA = [
             { type: "NEW", text: "레벨 15 달성 시 보스 출현 클리어 시 업적 달성" },
             { type: "BALANCE", text: "네온 소드마스터 분류의 직업 데미지 하향" },
             { type: "NEW", text: "구글 로그인 연동으로 DB구축 및 데이터 확보" },
+            { type: "NEW", text: "LEADER BOARD 기록 추가" },
             { type: "NEW", text: "육각형(엘리트)Enemy 처치 시 Gem 자석 아이템 드롭" }
         ]
     }
